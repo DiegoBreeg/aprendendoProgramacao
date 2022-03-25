e5ffd3828829fae14004d5d735696f01147c96e1
DiegoBreeg <diegobreeg@gmail.com>
2022-03-25 19:02:09 -0300

    master

--- estudosIndependentes/testes/main.js
+++ estudosIndependentes/testes/main.js
@@ -1,6 +1,27 @@
-import escreve from './module.js';
 
-var corpo = document.querySelector('#corpo');
 
-corpo.innerHTML += '<h4>[main.js] innerHTML executado com sucesso!</h4>';
-corpo.innerHTML += escreve();
+
+var btn = document.querySelector("#btn");
+btn.addEventListener('click', clicar);
+btn.addEventListener('mouseout', sair)
+btn.addEventListener('mouseenter', entrar)
+
+
+function clicar() {
+    var a = document.querySelector('#btn');
+    a.innerHTML = `clicou`;
+    a.style.background = 'red';
+}
+
+function sair() {
+    var a = document.querySelector('#btn');
+    a.innerHTML = 'Saiu';
+    a.style = 'blue'
+}
+
+function entrar() {
+    var a = document.querySelector('#btn');
+    a.innerHTML = 'Entrou'
+}
+
+
--- estudosIndependentes/testes/module.js
+++
@@ -1,3 +0,0 @@
-export default function echo(){    
-    return '<h4>[module.js] innerHTML executado com sucesso!</h4>'
-}

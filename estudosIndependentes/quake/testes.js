282b6423a2b10ff22ca79e74039d83e2e5c6684b
DiegoBreeg <diegobreeg@gmail.com>
2022-03-30 08:55:57 -0300

    master

--- estudosIndependentes/quake/testes.js
+++ estudosIndependentes/quake/testes.js
@@ -2,24 +2,14 @@ const fs = require('fs');
 var txt = fs.readFileSync('./Quake.txt', 'utf8');
 
 const linha = txt.split('\n');
-const killer = [];
 
+var killsLines = linha.filter((ell) => /kill:/gi.test(ell))
+killsLines = killsLines.map((ell, index) => ell.split(/\s+\d+:\d+\skill:\s\d+\s\d+\s\d+:\s/i)[1]);  
+killsLines = killsLines.map((ell, index) => ell.split(/\skilled/i)[0]);
 
-for(i = 0; i <= linha.length; i++) {
-    
-    if(/kill:/gi.test(linha[i])){
-        let getData = linha[i].split(/\s+\d+:\d+\skill:\s\d+\s\d+\s\d+:\s/i);
-        getData = getData[1].split(/\skilled/i);
-        killer.push(getData[0]);        
-    }
-}
-
-
-
-
-let killer2 = killer.filter((ell, index) => killer.indexOf(ell) == index);
+let killer2 = killsLines.filter((ell, index) => killsLines.indexOf(ell) == index);
 let resultado = killer2.map(ell => {
-     let data = killer.reduce((acc, ell2) => acc = ell2 == ell? acc + 1: acc + 0 ,0);
+     let data = killsLines.reduce((acc, ell2) => acc = ell2 == ell? acc + 1: acc + 0 ,0);
      return [ell, data];
 })
 console.log(resultado)

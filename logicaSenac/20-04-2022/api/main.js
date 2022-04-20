c4f083da6ce562134d11f20f0e003c5e4492c5dd
DiegoBreeg <diegobreeg@gmail.com>
2022-04-20 11:10:50 -0300

    master

---
+++ logicaSenac/20-04-2022/api/main.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 3000;
+
+app.get('/teste', (req, res) => res
+    .status(200)
+    .send({mensagem: 'Boas vindas à API'}))
+
+app.listen(port, () => console.log(`servidor está funcionando na porta ${port}`))
+
+module.exports = app;
